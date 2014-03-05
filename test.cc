e47bb203aefdb1106f170854cb676b27a2acbd7d
Dan Schatzberg <schatzberg.dan@gmail.com>
2014-03-05 09:37:36 -0500

    Get app linking

---
+++ test.cc
@@ -0,0 +1,13 @@
+//          Copyright Boston University SESA Group 2013 - 2014.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+#include <ebbrt/Debug.h>
+
+extern "C" int main(int argc, char **argv);
+
+void AppMain() {
+  const char* argv[] = { "node" };
+  main(1, const_cast<char**>(argv));
+}
