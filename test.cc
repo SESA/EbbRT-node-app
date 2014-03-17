cf50772f899e402ff6f470502a05ffaf14bc3f04
Dan Schatzberg <schatzberg.dan@gmail.com>
2014-03-17 10:49:33 -0400

    Get HTTP hello world working

--- test.cc
+++ test.cc
@@ -3,12 +3,16 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
+#include <cstdlib>
+#include <ctime>
+
 #include <ebbrt/Debug.h>
 
 extern "C" int main(int argc, char **argv);
 
 void AppMain() {
-  const char* argv[] = { "node" };
-  auto i = main(1, const_cast<char**>(argv));
+  putenv(const_cast<char *>("TZ=EST5EDT4,M3.2.0,M11.1.0"));
+  const char *argv[] = { "node" };
+  auto i = main(1, const_cast<char **>(argv));
   ebbrt::kprintf("Return Code: %d\n", i);
 }
